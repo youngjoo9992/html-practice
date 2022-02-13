5c111eba5428a51a8d23e641e199c369834791c3
youngjoo9992 <71207893+youngjoo9992@users.noreply.github.com>
2022-02-13 15:27:40 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,47 @@
 "use strict";
 
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    
+    
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}
 
-const home = (req, res) => {
-    res.render("home/index");
-};
+
+const users = {
+    id: ["test", "test2", "test3", "youngjoo9992"],
+    psword: ["testpw", "test2pw", "test3pw", "1234"],
+}
 
 
-const login = (req, res) => {
-    res.render("home/login");
+const process = {
+    login: (req, res) => {
+        const id = req.body.id, 
+        psword = req.body.psword;
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+
+            if (users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
 };
 
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
